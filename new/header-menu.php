3cca10b751e24e97febcae02c330d7d544c4ae7e
Saori Suzuki <80813223+suimenka@users.noreply.github.com>
2022-05-21 12:57:31 +0900

    新版(WordPress)
    Co-authored-by: “suimenka” <“saosuke24@gmail.com”>

---
+++ new/header-menu.php
@@ -0,0 +1,12 @@
+<header id="header">
+  <nav id="nav">
+    <h1 class="site-title"><a href="<?php echo home_url('/'); ?>">suimenka</a></h1>
+
+    <ul>
+      <li><a href="#about">About</a></li>
+      <li><a href="#service">Service</a></li>
+      <li><a href="#works">Works</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+</header>
