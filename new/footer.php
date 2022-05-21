f0cc68b6e6d2ec87e627ed3786d39bb160c98b36
“suimenka” <saosuke24@gmail.com>
2022-05-21 12:55:29 +0900

    新版(WordPress)

--- new/footer.php
+++ new/footer.php
@@ -1,9 +1,10 @@
 <footer id="footer">
-        <p class="copyright">&copy; <?php echo bloginfo('name'); ?></p>
-      </footer>
+  <p class="copyright">&copy; <?php echo bloginfo('name'); ?></p>
+</footer>
 
 <a href="#" class="pagetop">▲</a>
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
